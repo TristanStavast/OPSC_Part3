bbd0df9b3b0354cc9a595699fe3c5a49021dcd92
Tristan Stavast <stavasttristan@gmail.com>
2024-06-09 12:27:47 +0200

    Added stopwatch on timer page

--- app/src/main/java/com/example/opsc_part3/Timer.kt
+++ app/src/main/java/com/example/opsc_part3/Timer.kt
@@ -1,20 +1,139 @@
 package com.example.opsc_part3
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Timer : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    private var seconds = 0
+    private var running = false
+    private var wasRunning = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
         setContentView(R.layout.activity_timer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        val openDrawer : ImageButton = findViewById(R.id.btnNav)
+        openDrawer.setOnClickListener()
+        {
+            if (!drawerLayout.isDrawerOpen(navView)) {
+                drawerLayout.openDrawer(navView)
+            }
         }
+
+        //Intents
+        navView.setNavigationItemSelectedListener {
+            val timeint = Intent(this, TimesheetList::class.java)
+            val homeint = Intent(this, Home::class.java)
+            val logout = Intent(this, MainActivity::class.java)
+
+            when(it.itemId){
+                R.id.nav_home -> startActivity(homeint)
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
+                R.id.nav_report -> Toast.makeText(applicationContext, "Clicked Report", Toast.LENGTH_SHORT).show()
+                R.id.nav_timesheet -> startActivity(timeint)
+                R.id.nav_categories -> Toast.makeText(applicationContext, "Clicked Categories", Toast.LENGTH_SHORT).show()
+                R.id.nav_logout -> startActivity(logout)
+            }
+            true
+
+        }
+
+        var btnstart : Button = findViewById(R.id.btnStart)
+        var btnstop : Button = findViewById(R.id.btnStop)
+        var btnreset : Button = findViewById(R.id.btnReset)
+
+        if(savedInstanceState != null)
+        {
+            seconds = savedInstanceState.getInt("seconds")
+            running = savedInstanceState.getBoolean("running")
+            wasRunning = savedInstanceState.getBoolean("wasRunning")
+        }
+
+        runTimer()
+
+
+        btnstart.setOnClickListener()
+        {
+            running = true
+        }
+        btnstop.setOnClickListener()
+        {
+            running = false
+        }
+        btnreset.setOnClickListener()
+        {
+            running = false
+            seconds = 0
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("seconds", seconds)
+        outState.putBoolean("running", running)
+        outState.putBoolean("wasRunning", wasRunning)
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(wasRunning)
+        {
+            running = true
+        }
+    }
+
+    private fun runTimer()
+    {
+
+        var txtstopwatch : TextView = findViewById(R.id.txtStopwatch)
+
+        val handler = Handler()
+        handler.post(object : Runnable
+        {
+            override fun run()
+            {
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val secs = seconds % 60
+
+                val time = String.format("%02d:%02d:%02d", hours, minutes, secs)
+                txtstopwatch.text = time
+
+                if(running)
+                {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
     }
 }
