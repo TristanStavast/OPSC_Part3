972ec48e405fe7614955070abc104806e299a35f
aidenhemmings <agjhemmings@gmail.com>
2024-06-09 17:25:34 +0200

    fixed checkifexists class

--- app/src/main/java/com/example/opsc_part3/CheckIfExists.kt
+++ app/src/main/java/com/example/opsc_part3/CheckIfExists.kt
@@ -8,12 +8,12 @@ class CheckIfExists {
             return userList.any { it.username == username }
         }
 
-        fun isTimesheetExists(tsName: String, timesheetList: List<TimesheetData>): Boolean {
-            return timesheetList.any { it.tsName == tsName }
+        fun isTimesheetExists(username: String, tsName: String, timesheetList: List<TimesheetData>): Boolean {
+            return timesheetList.any { it.username == username && it.tsName == tsName }
         }
 
-        fun isCategoryExists(categoryName: String, categoryList: List<CategoryData>): Boolean {
-            return categoryList.any { it.CategoryName == categoryName }
+        fun isCategoryExists(username: String, categoryName: String, categoryList: List<CategoryData>): Boolean {
+            return categoryList.any { it.username == username && it.CategoryName == categoryName }
         }
 
     }
