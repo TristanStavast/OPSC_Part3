97a1848a8f36146f51c2e8240b82c98266b140d3
AlexiGiagas <alexi.giagas@gmail.com>
2024-06-09 16:23:46 +0200

    reports

---
+++ app/src/main/java/com/example/opsc_part3/Reports.kt
@@ -0,0 +1,59 @@
+package com.example.opsc_part3
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class Reports : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        setContentView(R.layout.activity_profile)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        val openDrawer : ImageButton = findViewById(R.id.btnNav)
+        openDrawer.setOnClickListener()
+        {
+            if (!drawerLayout.isDrawerOpen(navView)) {
+                drawerLayout.openDrawer(navView)
+            }
+        }
+
+        //Intents
+        navView.setNavigationItemSelectedListener {
+            val timeint = Intent(this, TimesheetList::class.java)
+            val profile = Intent(this, Profile::class.java)
+            val homeint = Intent(this, Home::class.java)
+            val categint = Intent(this, Categories::class.java)
+            val logout = Intent(this, MainActivity::class.java)
+
+            when(it.itemId){
+                R.id.nav_home -> startActivity(homeint)
+                R.id.nav_profile -> startActivity(profile)
+                R.id.nav_report -> Toast.makeText(applicationContext, "Clicked Report", Toast.LENGTH_SHORT).show()
+                R.id.nav_timesheet -> startActivity(timeint)
+                R.id.nav_categories -> startActivity(categint)
+                R.id.nav_logout -> startActivity(logout)
+            }
+            true
+
+        }
+    }
+    override fun onBackPressed() {
+
+    }
+}
